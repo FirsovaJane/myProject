91e68ceaa78a50bf70ab705aa0415ffa1945ec86
FirsovaJane <firsova-z@mail.ru>
2016-04-02 12:26:11 +0500

    Update WSGIMiddleware.py

--- WSGIMiddleware.py
+++ WSGIMiddleware.py
@@ -1,16 +1,6 @@
 TOP = "<div class='top'>Middleware TOP</div>"
 BOTTOM = "<div class='botton'>Middleware BOTTOM</div>"
 
-def app(environ, start_response): 
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    path = environ['PATH_INFO'] # считываем путь 
-	# анализируем путь и корректируем его
-    pathResult = '/' + path 
-    if pathResult == '/':
-            pathResult ='/index.html' 
-    file = open(pathResult, 'r')
-    return [file.read()]
-
 class WSGIMiddleware(app): # Прослойка middleware
     def __init__(self, app): 
         self.app = app
@@ -22,20 +12,33 @@ class WSGIMiddleware(app): # Прослойка middleware
 		# послее вставляя BOTTOM перед </body>
 		# с помощью оразделителя split
         if (resultHTML.find('<body>') >= 0):	 
-        	# делим html страницу на три части - до <body>, содержимое самого тела и после </body>
+        	# делим html страницу на три части:
+        	# до <body> - beginHTML
+                # содержимое самого тела - bodyHTML
+                # после </body> - endHTML
             beginHTML, bodyHTML	= 	resultHTML.split('<body>')
-            bodyHTML, endHTML 	= 	bodyHTML.split('<body>')
+            bodyHTML, endHTML 	= 	bodyHTML.split('</body>')
             resultHTML = beginHTML + '\n<body>\n' + TOP + '\n' + bodyHTML + '\n' + BOTTOM + '\n</body>\n' + endHTML # add TOP, BOTTOM
+		else:
 		# если страница не иммет body, то мы сначала добавляем перед </html> само тело 
 		# и лишь потом в body добавляем TOP и BOTTOM
-		# с помощью оразделителя split		
-		else:
+		# с помощью оразделителя split	
 			beginHTML, endHTML = resultHTML.split('</html>')
 			resultHTML = beginHTML + '\n<body>\n' + TOP + '\n' + BOTTOM + '\n</body>'+ '\n</html>'
-		# возвращаем полученный результат - страница html со вставленныйми в него TOP и BOTTOM
+	# возвращаем полученный результат - страница html со вставленныйми в него TOP и BOTTOM
         return resultHTML
 
-app = WSGIMiddleware(app) # Прослойка middleware
+def app(environ, start_response): 
+    start_response('200 OK', [('Content-Type', 'text/html')])
+    path = environ['PATH_INFO'] # считываем путь 
+	# анализируем путь и корректируем его
+    pathResult = '/' + path 
+    if pathResult == '/':
+            pathResult ='/index.html' 
+    file = open(pathResult, 'r')
+    return [file.read()]
+
+app = WSGIMiddleware(app) 
 
 if __name__ == '__main__':
     from paste.httpserver import serve
